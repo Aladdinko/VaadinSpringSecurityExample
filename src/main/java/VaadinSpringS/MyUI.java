1929a05daca67594ec09f22b671dde61bf2b0876
Maggouh <alaerifi-85>
2017-01-17 13:01:14 +0100

    test

--- src/main/java/VaadinSpringS/MyUI.java
+++ src/main/java/VaadinSpringS/MyUI.java
@@ -7,11 +7,7 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window
- * (or tab) or some part of a html page where a Vaadin application is embedded.
- * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
- * overridden to add component to the user interface and initialize non-component functionality.
+ * Created by maggouh on 10/01/17.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
